205454340678ad4cabed73660bb6a71474d51f90
krypton-byte <rosid6434@gmail.com>
2024-08-17 00:46:01 +0700

    global ip

--- app/__main__.py
+++ app/__main__.py
@@ -16,7 +16,7 @@ def start():
         "app:app",
         reload=True,
         reload_dirs=Path(__file__).parent.__str__(),
-        host=os.environ.get("HOST", "127.0.0.1"),
+        host=os.environ.get("HOST", "0.0.0.0"),
         port=int(os.environ.get("PORT", 8000)),
     )
 
